ee24b8fee8313c39e65ef1e669bb731f3c14cfcc
andy19910102 <andy19910102@gmail.com>
2015-12-28 00:45:05 +0800

    logged in change in flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:password])
         session[:user_id] = user.id
-        flash[:success] = "Hello, #{current_user.name}! You've logged in."
+        flash[:info] = "Hello, #{current_user.name}! You've logged in."
         redirect_to root_path
     else
       flash[:danger] = "Fail to logged in. Please try again."
